30052ac5d222710c412d22b5dd93459d16ec070f
Yujeeb Abbas <k.yujeeb@gmail.com>
2024-03-21 20:46:50 +0530

    Improved the bot

--- gemini_chat.py
+++ gemini_chat.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-from utils import save_private_chat, get_chat_history
+from utils import save_chat, get_chat_history
 import os
 from constants import GEMINI_API_KEY
 
@@ -8,49 +8,36 @@ class GeminiChat():
     def __init__(self):
         genai.configure(api_key=GEMINI_API_KEY)
         self.model = genai.GenerativeModel('gemini-pro')
-        PROMPT = "Respond as a human named BotSon."
-        self.messages = [{
-            'role': 'user',
-            'parts': PROMPT
-        },
-        {
-            'role': 'model',
-            'parts': 'Okay. My name is BotSon.'
-        },
-        {
-            'role': 'user',
-            'parts': 'Your developer is Yujeeb Abbas.'
-        },
-        {
-            'role': 'model',
-            'parts': 'Okay. My developer is Yujeeb Abbas.'
-        }]
 
 
     def message(self, username, chat_name, message_type, input_text: str) -> str:
         filename = "chats/"  + str(message_type) + "/" + str(chat_name) + ".csv"
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-
-        self.messages += get_chat_history(username, filename)
+        
+        chat_history = get_chat_history(username, filename)
+        
         user_message = {'role': 'user',
                         'parts': input_text.replace("\n", " ")}
         
-        self.messages.append(user_message)
+        chat_history.append(user_message)
         print("added user message")
-        
-        response = self.model.generate_content(self.messages)
 
-        self.messages[-1]['username'] = username
+        response = self.model.generate_content(chat_history)
+        print("generated response")
+        
+        user_message['username'] = username
         
         model_response = {'role': 'model',
                           'username': username,
                           'parts': response.text.replace("\n", " ")}
         
-        self.messages.append(model_response)
+        batch = []
+
+        batch.append(user_message)
+        batch.append(model_response)
         print("added model response")
 
-        print(self.messages)
+        # print(chat_history)
 
-        save_private_chat(filename, self.messages)        
+        save_chat(username, filename, batch)        
 
         return response.text
--- utils.py
+++ utils.py
@@ -1,13 +1,33 @@
 import csv
+import os
 
 
-def save_private_chat(filename, chat_history):
-    with open(filename, 'w') as csvfile:
+def save_chat(username, filename, chat_history):
+    with open(filename, 'a') as csvfile:
         fieldnames = ['role', 'username', 'parts']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-        writer.writeheader()
+        if get_chat_history(username, filename) == []:
+            chat_initialization = [{
+                'role': 'user',
+                'parts': "Respond as a human named BotSon."
+            },
+            {
+                'role': 'model',
+                'parts': 'Okay. My name is BotSon.'
+            },
+            {
+                'role': 'user',
+                'parts': 'Your developer is Yujeeb Abbas.'
+            },
+            {
+                'role': 'model',
+                'parts': 'Okay. My developer is Yujeeb Abbas.'
+            }]
 
+            writer.writeheader()
+            writer.writerows(chat_initialization)
+        
         writer.writerows(chat_history)
 
 
@@ -18,9 +38,9 @@ def get_chat_history(username, filename):
         with open(filename, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
 
-        for data in reader:
-            if data['username'] in (username, ''):
-                chat_history.append({'role': data['role'], 'parts': data['parts']})
+            for data in reader:
+                if data['username'] in (username, ''):
+                    chat_history.append({'role': data['role'], 'parts': data['parts']})
     except:
         pass
 
