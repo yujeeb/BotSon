dfb07e65d9dba1e9e8a66fdb2fc9756c9748970b
yujeeb <87360190+yujeeb@users.noreply.github.com>
2024-03-21 17:52:28 +0530

    Delete telegram_bot directory

--- telegram_bot/test.py
+++
@@ -1,8 +0,0 @@
-from langchain_core.messages import HumanMessage
-from langchain_mistralai.chat_models import ChatMistralAI
-
-# If mistral_api_key is not passed, default behavior is to use the `MISTRAL_API_KEY` environment variable.
-chat = ChatMistralAI(mistral_api_key="")
-
-messages = [HumanMessage(content="knock knock")]
-chat.invoke(messages)
